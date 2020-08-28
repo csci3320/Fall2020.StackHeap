389124a3ce176cd00093576b16e5df13326d3179
Ricks <bricks@unomaha.edu>
2020-08-28 13:04:39 -0500

    Create MyInt.java

---
+++ MyInt.java
@@ -0,0 +1,15 @@
+
+public class MyInt {
+
+	public int amount;
+	
+	public MyInt(int i) {
+		this.amount = i;
+	}
+
+	public void addTen() {
+		this.amount = this.amount + 10; //amount+=10;
+		
+	}
+
+}
